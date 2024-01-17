a07733b07901378c7efe402211a1ec0048a67898
Iam-shyamrathore <118192246+Iam-shyamrathore@users.noreply.github.com>
2024-01-17 15:44:32 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,7 @@
-
+create database employemanagement;
+use employemanagement;
+create table login(username varchar(20), password varchar(20));
+select*from login;
+insert into login values('shyam','12345678');
+create table employee(name varchar(40),fname varchar(40),dob varchar(40),salary varchar(40),adress varchar(40),phone varchar(40),email varchar(40),education varchar(40),designation varchar(40),aadhar varchar(40),empid varchar(40));
+select*from employee;
